53ea4234b5d8d2b2ef2189e68be5428c94da4bdd
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2014-02-04 20:48:18 -0500

    Added script to make home folder links

---
+++ shell/linkHomeFiles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Creates links in home to folders on Data1
+
+ln -s /Data1/ryan/Documents ~/Documents
+ln -s /Data1/ryan/Downloads ~/Downloads
+ln -s /Data1/ryan/Music ~/Music
+ln -s /Data1/ryan/Pictures ~/Pictures
+ln -s /Data1/ryan/Videos ~/Videos
+ln -s /Data1/ryan/Dropbox ~/Dropbox
+ln -s /Data1/ryan/scripts ~/scripts
