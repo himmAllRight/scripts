1b59a9392d5a04e950d9691ca40aa22aa0d5da52
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2014-06-01 11:03:41 -0400

    Working on is palendrome function: need to fix it so that the middle most numbers are tested in even sequences. Also need to figure out how to use for numbers then

---
+++ euler_solutions/problem4.py
@@ -0,0 +1,24 @@
+def isPalendrome(num):
+	""" Reads an input and determines if it is a pallendrome """
+	i 	= 0
+	j 	= len(num) - 1
+	mid = len(num) // 2
+
+	print(mid)
+	
+	# While i and j are not in the middle
+	while( i != mid and j != mid):
+		print(i,j,sep="\t")
+		print(num[i],num[j], sep="\t")
+		if(num[i] != num[j]):
+			return(False)
+		else:
+			i = i + 1
+			j = j - 1
+
+	return(True)
+
+
+
+## Execution Code ##
+print(isPalendrome('acka'))
