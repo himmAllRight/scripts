38325fd18e81bc09d324555464655d5ee7bebc56
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2014-06-01 12:07:14 -0400

    Solved. Simple solution, but it still returned the answer in 0.0s in Sublime Text build...

---
+++ euler_solutions/problem6.py
@@ -0,0 +1,26 @@
+def getFirstNumber(didg):
+	""" Returns the square of sums """
+	value = 0
+	for x in range(1,didg + 1):
+		value = value + x
+
+	return(value**2)
+
+def getSecondNumber(didg):
+	""" Returns the sum of squares """
+	value = 0
+
+	for x in range(1, didg + 1):
+		value = value + (x**2)
+	return(value)
+
+
+
+## Execution Code ##
+
+didget = 100
+
+n1 = getFirstNumber(didget)
+n2 = getSecondNumber(didget)
+
+print(n1 - n2)
