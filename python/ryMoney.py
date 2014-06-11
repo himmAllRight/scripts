ca3b50675ebd03cfe96c09e75338fe6a61b92339
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2014-06-11 00:37:47 -0400

    Made some Progress on simple checking functionality Need to write a CUI logic, and add read/write functionality for a simple checking. Then, I can move onto the budeting functionality of the program

--- python/ryMoney.py
+++ python/ryMoney.py
@@ -1,21 +1,57 @@
+import time
+
+class Categories:
+	""" A payment category """
+	def __init__(self):
+		self.list = []
+
+	def addCategory(self, categoryName):
+		## Adds new Category if it  does not exist
+		if(self.list.count(categoryName) == 0):
+			self.list.append(categoryName)
+		else:
+			print("Category ", categoryName, " already exists!", sep="")
+
+	def containsCategory(self, searchName):
+		""" Returns the index of a searched category, otherwise returns -1 """
+		if(self.list.count(searchName) > 0):
+			return(self.list.index(searchName))
+		else:
+			return(-1)
+
+	def removeCategory(self, deleteName):
+		if(self.list.count(deleteName) > 0):
+			self.list.remove(deleteName)
+
+	def printCategories(self):
+		print("Categories:")
+		id = 0
+		for cat in self.list:
+			print(id, ":", cat, "  ", end="")
+			id = id + 1
+		print("\n")
+
 class Account:
 	""" Bank Account Class """
-	def __init__(self, accountName):
+	def __init__(self, accountName, catList):
 		self.name 	      = accountName
 		self.balance      = 0
 		self.transactions = []
+		self.categories   = catList
 
-	def newDeposit(self, name, amount):
+	def newDeposit(self, name, catInd, amount):
 		""" Adds a new deposit to account. """
 		self.balance = self.balance + amount
 
 		# Adds new Deposit to Account
-		self.transactions.append(Transaction(name, "D", amount))
+		self.transactions.append(Transaction(name, " DEP ", self.categories.list[catInd] ," _ ", amount, 
+								 self.balance))
 
-	def newWithdrawl(self, name, amount):
+	def newWithdrawl(self, name, catInd, num, amount):
 		""" Takes a new new Withdrawl from the account """
 		self.balance = self.balance - amount
-		self.transactions.append(Transaction(name, "W", amount*(-1)))
+		self.transactions.append(Transaction(name, num, self.categories.list[catInd], " _ ", amount*(-1), 
+								 self.balance))
 
 	def printAccountInfo(self):
 		""" Prints out the account information """
@@ -24,27 +60,47 @@ class Account:
 
 		print("Transactions:", "\n---------------",)
 
-		i = 1
-		for tranaction in self.transactions:
-			print(i, tranaction.type, tranaction.name, tranaction.amount, sep="\t")
-			i = i + 1
+		print("Date", "Num", "Payee", "Category", "Cleared", "Amount", 
+			  "Balance", sep="\t\t")
+		
+		for transaction in self.transactions:
+			print(transaction.date, "\t\t", transaction.num, "\t\t", 
+				  transaction.name, "\t\t", transaction.category ,"\t\t",
+				  transaction.cleared, "\t\t", "$", transaction.amount,"\t\t",
+				  "$", transaction.balance, 
+				sep="")
 
 
 class Transaction:
 	""" Transaction for accounts """
-	def __init__(self, tranactionName, tranactionType, tranactionAmount):
-		self.name 	= tranactionName
-		self.type	= tranactionType
-		self.amount	= tranactionAmount
+	def __init__(self, transactionName, transactionNum, transactionCatigory,
+				 transactionCleared, transactionAmount, newBalance):
+
+		self.name 	  = transactionName
+		self.date     = time.strftime("%d/%m/%Y")
+		self.num	  = transactionNum
+		self.category = transactionCatigory
+		self.cleared  = transactionCleared
+		self.amount	  = transactionAmount
+		self.balance  = newBalance
 
 class __main__:
 	""" The Main Class """
 	print("Main Class")
 
 	## Test Execution Code ##
-	account = Account("Checking")
-	account.newDeposit("Income", 500)
-	account.newDeposit("Gift", 50)
-	account.newDeposit("Income2", 613)
-	account.newWithdrawl("Bills", 63)
+	deer = Categories()
+	deer.addCategory("Rent")
+	deer.addCategory("Pay")
+	deer.addCategory("Bills")
+	deer.addCategory("Other")
+
+
+	account = Account("Checking", deer)
+	deer.printCategories()
+
+	account.newDeposit("Income", 1, 500)
+	account.newDeposit("Gift", 3, 50)
+	account.newDeposit("Income2", 2, 613)
+	account.newWithdrawl("Bills", 2, "02132", 63)
 	account.printAccountInfo()
