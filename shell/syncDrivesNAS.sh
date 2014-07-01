a1e254c877cd5f73bff31a72b19ab2c635a1bc79
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2014-07-01 14:22:17 -0400

    removed cache clearing to own script

--- shell/syncDrivesNAS.sh
+++ shell/syncDrivesNAS.sh
@@ -5,6 +5,3 @@ echo $(date) >> /Data1/syncLog.txt
 echo >> /Data1/syncLog.txt
 cp /Data1/syncLog.txt /Data2/
 cp /Data1/syncLog.txt /Data1/ryan/Dropbox/
-
-# Clear mem cache after sync
-sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
