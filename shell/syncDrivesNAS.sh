f7f6d514bf62f62f936b725edaa32da8a1df0600
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2014-07-01 14:18:51 -0400

    Added cache clearing

--- shell/syncDrivesNAS.sh
+++ shell/syncDrivesNAS.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo $(date) >> /Data1/syncLog.txt
-rsync -avzh --progress --delete /Data1/ryan/ /Data2/ryan/
+rsync -avzh --progress --delete /Data1/ryan/ ryan@192.168.1.2:/mnt/himmelwrightData/ryan/Desktop
 echo $(date) >> /Data1/syncLog.txt
 echo >> /Data1/syncLog.txt
 cp /Data1/syncLog.txt /Data2/
