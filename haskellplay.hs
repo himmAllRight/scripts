b23a77ebb70123d2f892904dcbf6ccb95a697644
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2014-12-19 10:36:03 -0500

    Working on euler problem74 actually... almost done brute solution...

--- haskellplay.hs
+++ haskellplay.hs
@@ -7,3 +7,18 @@ didgetL :: Int -> [Int] -> [Int]
 didgetL n l
   | quot n 10 <= 0	= reverse(l ++ [n])
   | otherwise	 	= didgetL (quot n 10) (l ++ [n `mod` 10])
+  
+-- A function that returns the summed factorials of a Int list
+sumFactList :: [Int] -> Int
+sumFactList l = sum (map factorial l)
+
+-- A function that returns the sum-factorial chain of a number
+returnChain :: [Int] -> [Int]
+returnChain l = if head l `elem` tail l then l else returnChain ([sumFactList (didgetL (head l) [] )] ++ l)
+
+-- Checks if a number is a certain number
+isNum :: Int -> Int
+isNum 60 = 1
+isNum  n = 0
+
+
