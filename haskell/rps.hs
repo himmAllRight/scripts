58313b4e45e6b16a0b03312f27cfc8d16769e521
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2015-04-30 07:59:23 -0400

    Added a number2RPS function to convert ints to RPS values, and another that maps over an int list to make an RPS list. Need to get the random generators working, and combine with this to make a random case.

--- haskell/rps.hs
+++ haskell/rps.hs
@@ -1,5 +1,6 @@
 -- Rock Paper Scissors game in haskell. Will use for more advanced stuff later.
-
+import Data.Time
+import System.Random
 data RPS = Rock | Paper | Scissors deriving (Eq, Show, Read)
 type Score = (Int, Int)
 
@@ -19,3 +20,15 @@ multiPlay :: [RPS] -> [RPS] -> Score -> Score
 multiPlay [] b (sa,sb) = (sa,sb)
 multiPlay a [] (sa,sb) = (sa, sb)
 multiPlay (a:ax) (b:bx) (sa,sb) = multiPlay ax bx (play a b (sa,sb))
+
+num2RPS :: Int -> RPS
+num2RPS x = rpsReturn
+    where
+    rpsReturn
+      | x == 0    = Rock
+      | x == 1    = Paper
+      | otherwise = Scissors
+
+numList2RPS :: [Int] -> [RPS]
+numList2RPS n = map num2RPS n
+
