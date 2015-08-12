7d4bf201e30e1803838d8b86b38b5dd04614b69d
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2015-08-12 07:23:56 -0400

    Updated video script to actually work using youtube-dl. This means that it will both download and convert the files, and move them to a specified output location.

--- python/videoConvertScript.py
+++ python/videoConvertScript.py
@@ -4,18 +4,24 @@ import sys
 def f(string):
     return string.strip()
 
-inputFile=  open("/usr/home/ryan/Desktop/convertList.txt", "r")
-scriptFile= open("/usr/home/ryan/Desktop/.convertScript", "w")
-songName = ""
-songLink = ""
+fileType      = "mp3"
+inputFileLoc  = "/home/ryan/owncloud/convertList.txt"
+scriptFileLoc = "/home/ryan/owncloud/.convertScript"
+outputLoc     = "/home/ryan/Downloads/mps/"
+inputFile     = open(inputFileLoc, "r")
+scriptFile    = open(scriptFileLoc, "w")
+songName      = ""
+songLink      = ""
 count = 0
 for line in inputFile:
     count = count + 1
     if(count % 2 == 0):
         songLink =f(line)
         print(songName + " " + songLink)
-        printString= 'youtube2mp3 "' +songLink+ '" "' +songName+'.mp3"'
+        printString= 'youtube-dl ' +songLink+ ' -x --audio-format "' + fileType + '" -o "'  +songName+'.mp3"'
 #        outfile = open("convertScript.sh", "w")
+        print(printString, file= scriptFile)
+        printString= 'mv "'+ songName+'.mp3" "' + outputLoc + songName + '.mp3"'
         print(printString, file= scriptFile)
         print(count)
         print("--")
@@ -24,9 +30,10 @@ for line in inputFile:
         
 inputFile.close()
 scriptFile.close()
-os.system('chmod +x .convertScript')
-os.system('./.convertScript')
-os.system('rm .convertScript')
+cmd = "chmod +x " + scriptFileLoc
+os.system(cmd)
+os.system(scriptFileLoc)
+#os.system('rm .convertScript')
 
 
 
