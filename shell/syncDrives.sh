3e251dca1f0add564192ccb7fb40f8006e2619d4
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2015-10-27 07:51:01 -0400

    Edited path to sync entire /Data drives and not just /Data/ryan paths, since we now have out music, photos, and video files outside my folder, as well as rebecca's file

--- shell/syncDrives.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo $(date) >> /Data1/syncLog.txt
-rsync -avzh --progress --delete /Data1/ /Data2/
-echo $(date) >> /Data1/syncLog.txt
-echo >> /Data1/syncLog.txt
-cp /Data1/syncLog.txt /Data2/
