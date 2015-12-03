3c382f2c0ac241c27f61b9facb06cee9c7ea7ef6
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2015-12-02 22:03:44 -0500

    Playing around with what a carriage return actually means

--- C/C_The_Hard_Way/ex3.c
+++ C/C_The_Hard_Way/ex3.c
@@ -9,6 +9,8 @@ int main()
   printf("I am %d years old.\n", age);
   printf("I am %d inches tall.\n", height);
   printf("Carriage Return? I'm not 40! \r New line \n Vertical Tab?\v alarm?\a name: %s\n", "Ryan");
+  printf("333\r22\r1\n");
 
   return 0;
 }
+
