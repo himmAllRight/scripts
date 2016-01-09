8ffe5009df5f753d93dd51d79124c28105c53642
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2016-01-09 14:52:58 -0500

    Added player names to board. I want to clean it up a little bit to remove the redunant parts of the printGameBoard, and then I will move onto the game logic :)

--- ruby/mancala.rb
+++ ruby/mancala.rb
@@ -52,15 +52,16 @@ end
 
 
 def printGameBoard(p1, p2, boardSize)
-  border     = "-" * (((boardSize * 6) + 1) + 18)
-  p1_row     = "|" + p1.score.to_s.center(8) + p1.board_string + (" " * 8) + "|"
-  p2_row     = "|" + (" " * 8) + p2.board_string + p2.score.to_s.center(8) + "|"
-  mid_row    = "|" + (" " * 8) + ("-" * ((boardSize * 6) + 1)) + (" " * 8) + "|"
-  p1_labels  =  (" " * 10) + p1.board_labels + (" " * 9)
-  p2_labels  =  (" " * 10) + p2.board_labels + (" " * 9)
-   
+  left_buffer = (" " * (p1.name.length + 1))
+  border      = left_buffer + "-" * (((boardSize * 6) + 1) + 18)
+  p1_row      = p1.name + " |" + p1.score.to_s.center(8) + p1.board_string + (" " * 8) + "|"
+  p2_row      = left_buffer + "|" + (" " * 8) + p2.board_string + p2.score.to_s.center(8) + "|" + " " + p2.name
+  mid_row     = left_buffer + "|" + (" " * 8) + ("-" * ((boardSize * 6) + 1)) + (" " * 8) + "|"
+  p1_labels   =  left_buffer + (" " * 10) + p1.board_labels + (" " * 9)
+  p2_labels   =  left_buffer + (" " * 10) + p2.board_labels + (" " * 9)
+  
   print(p1_labels, "\n", border, "\n", p1_row, "\n", mid_row, "\n", p2_row, "\n", border, "\n", p2_labels, "\n")
-    
+  
 end
 
 
@@ -75,3 +76,15 @@ boardSize          = 6
 initialMarbelCount = 4
 
 boardIndexHash = createBoardIndex(boardSize)
+
+
+# Test run
+p1 = Player.new("Ryan", 6, 4, true)
+p2 = Player.new("Rebecca", 6, 4, false)
+
+p1.board[1] = 7
+p2.board[1] = 6
+
+puts "This is just a test printout of an example board."
+puts "Going to add the game logic next :)\n\n"
+printGameBoard(p1, p2, 6)
