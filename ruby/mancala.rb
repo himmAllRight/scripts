753aa817ad464727de9cb6239f94703f7e155a5b
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2016-01-09 12:24:10 -0500

    got basic board printing working. Might add user names still, but I have yet to determine where I want to place those

--- ruby/mancala.rb
+++ ruby/mancala.rb
@@ -1,10 +1,11 @@
 ## Class Defs
 class Player
   def initialize(name, boardSize, initialMarbelCount, reverseP)
-    @name    = name
-    @board   = Array.new(boardSize, initialMarbelCount)
-    @score   = 0
-    @reverseP = reverseP
+    @name      = name
+    @board     = Array.new(boardSize, initialMarbelCount)
+    @boardSize = boardSize
+    @score     = 0
+    @reverseP  = reverseP
   end
 
   # Accessor methods
@@ -43,6 +44,17 @@ class Player
     end
     return returnString
   end
+
+
+  def board_labels ()
+    if @reverseP
+      ('a'..'z').to_a.take(@boardSize).reverse.map { |n| n.to_s.center(5)}.join(" ")
+
+  else
+    ('a'..'z').to_a.take(@boardSize).map { |n| n.to_s.center(5)}.join(" ")
+    end
+  end
+
 end
 
 
@@ -59,12 +71,16 @@ end
 
 
 def printGameBoard(p1, p2, boardSize)
-  border  = "-" * (((boardSize * 6) + 1) + 18)
-  p1_row  = "|" + p1.score.to_s.center(8) + p1.board_string + (" " * 8) + "|"
-  p2_row  = "|" + (" " * 8) + p2.board_string + p2.score.to_s.center(8) + "|"
-  mid_row = "|" + (" " * 8) + ("-" * ((boardSize * 6) + 1)) + (" " * 8) + "|"
+  border     = "-" * (((boardSize * 6) + 1) + 18)
+  p1_row     = "|" + p1.score.to_s.center(8) + p1.board_string + (" " * 8) + "|"
+  p2_row     = "|" + (" " * 8) + p2.board_string + p2.score.to_s.center(8) + "|"
+  mid_row    = "|" + (" " * 8) + ("-" * ((boardSize * 6) + 1)) + (" " * 8) + "|"
+  p1_labels  =  (" " * 10) + p1.board_labels + (" " * 9)
+  p2_labels  =  (" " * 10) + p2.board_labels + (" " * 9)
+   
 
-  print(border, "\n", p1_row, "\n", mid_row, "\n", p2_row, "\n", border)
+
+  print(p1_labels, "\n", border, "\n", p1_row, "\n", mid_row, "\n", p2_row, "\n", border, "\n", p2_labels, "\n")
     
 end
 
@@ -80,6 +96,9 @@ boardSize          = 6
 initialMarbelCount = 4
 
 
-boardIndexHash = createBoardIndex(boardSize)  
+boardIndexHash = createBoardIndex(boardSize)
 puts boardIndexHash["b"]
 
+testStr = "Ryan" +
+          " " +
+          "Himmelwright"
