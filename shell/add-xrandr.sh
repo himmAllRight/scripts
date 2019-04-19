f408c4fdcb2632f1e6cb087c3369ded01591cc59
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2019-04-19 15:10:25 -0400

    I think I got this working...

--- shell/add-xrandr.sh
+++ shell/add-xrandr.sh
@@ -3,11 +3,23 @@
 WIDTH=$1
 HEIGHT=$2
 MONITOR=$3
+ESLEEP=3
 
 gtf_output=$(gtf $WIDTH $HEIGHT 59.9 | grep -i "modeline" | sed -e 's/\<Modeline\>//g')
-echo $gtf_output
+# echo $gtf_output
 modename=$(echo $gtf_output | grep -o  "\(\".*\"\)")
-echo $modename
+# echo $modename
+modeexists=$(xrandr | grep -i $modename)
 
-xrandr --newmode $gtf_output
-xrandr --addmode $MONITOR "1920x1200_59.90"
+if [ "$modeexists" == "" ]
+then
+	echo "xrandr --newmode $gtf_output"
+	xrandr --newmode $gtf_output
+	#echo "xrandr --addmode $MONITOR $modename"
+	#xrandr --addmode $MONITOR $modename
+else
+	echo "Hmmm... I think that mode already exists. Verify the xrandr output for me?"
+	echo "Don't worry. I'll print it in $ESLEEP seconds..."
+	sleep $ESLEEP
+	xrandr
+fi
