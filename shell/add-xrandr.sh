8f7387f438e5ad52cdcf3d0cef04056fdfd01505
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2019-04-19 15:24:32 -0400

    Added ability to switch right away

--- shell/add-xrandr.sh
+++ shell/add-xrandr.sh
@@ -11,12 +11,29 @@ modename=$(echo $gtf_output | grep -o  "\(\".*\"\)")
 # echo $modename
 modeexists=$(xrandr | grep -i $modename)
 
+
+# A function to prompt the user if they want to switch to the new mode now.
+switch_to_new_mode () {
+	echo -n "Switch to new mode now? [y/n]: "
+	read change
+	if [ "$change" == "y" ]
+	then
+		echo "switching monitor..."
+		xrandr --output $MONITOR --mode $modename
+	else
+		echo "Okay, no switch. Enjoy!"
+	fi
+}
+
+
 if [ "$modeexists" == "" ]
 then
-	echo "xrandr --newmode $gtf_output"
+	echo "Adding new mode: $gtf_output"
 	xrandr --newmode $gtf_output
-	#echo "xrandr --addmode $MONITOR $modename"
-	#xrandr --addmode $MONITOR $modename
+	echo "Adding new mode [$modename] to display [$MONITOR]"
+	xrandr --addmode $MONITOR $modename
+	echo "Done!"
+	switch_to_new_mode
 else
 	echo "Hmmm... I think that mode already exists. Verify the xrandr output for me?"
 	echo "Don't worry. I'll print it in $ESLEEP seconds..."
